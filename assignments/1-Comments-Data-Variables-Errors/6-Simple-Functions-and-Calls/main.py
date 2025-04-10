09508443a8b04e9eb0c17ef266f68ca0ad64bb28
x702 <x702@localhost.localdomain>
2025-04-10 02:24:15 -0500

    simple-functions tests

--- assignments/1-Comments-Data-Variables-Errors/6-Simple-Functions-and-Calls/main.py
+++ assignments/1-Comments-Data-Variables-Errors/6-Simple-Functions-and-Calls/main.py
@@ -1,5 +1,56 @@
 
-def add(a,b):
-    return a + b
 
-twosum = add(6,4)
+
+
+
+
+
+def subtract(minuend,subtrahend):
+    return minuend + subtrahend
+
+twodiff = subtract(1024,512)
+print(twodiff)
+
+
+def multiply(a,y):
+    return a * y
+
+twoproduct = multiply(128,8)
+print(twoproduct)
+
+def divide(dividend, d):
+    return dividend / d
+
+twoquotient = divide(65536,10)
+print(twoquotient)
+
+def concatenate(front_half,back_half):
+    return front_half + back_half
+
+
+j = concatenate("It means ", "to join together.")
+print(j)
+
+
+
+print("Its", "not", "a", "bug","its", "a", "feature")
+
+
+print("First","Second","Third","Fourth", sep = "|")
+
+
+print("Its ", end = "")
+print("a ",end="")
+print("twist ",end="")
+print("ENDing.",end="")
+
+num = int(input("Enter a number:\n"))
+print(num * 5)
+
+flo = float(input("Enter a number:\n"))
+print(flo * 3.14)
+
+
+
+print(len("abcdefghijklmnopqrstuvwxyz"))
+print(len("a b c d e f g h i j k l m n o p q r s t u v w x y z "))
--- assignments/1-Comments-Data-Variables-Errors/6-Simple-Functions-and-Calls/test.py
+++ assignments/1-Comments-Data-Variables-Errors/6-Simple-Functions-and-Calls/test.py
@@ -1,4 +1,4 @@
-identity='71d382fe51af4db67ab8984803d7221a53676eb8a4aac8aafac46cbdbd9abb278dd70f2590b04c09b518ef557506bbdf85dbe9de9679e99ce7781a11603d9cae'
+identity='d52396689d3b9d3f10c26dd290533fc6c07f6bffea94dbe2c89c2c2582e31f7cc5afd3830c9ef2d7f935d9055d2ab74fffd64b88ecbda159426e46a9ebf2dff9'
 import subprocess,os,sys,hashlib
 from datetime import datetime
 from getpass import getuser
@@ -22,8 +22,8 @@ def maketests(m=None):
 
     def isfunction(qnum,name):
 
-        assert hasattr(m,name), f"Q{qnum}: Did you define a function named {name}? Did you use the 'def' keyword? Did you use parenthesis?"
-        assert callable(getattr(m,name)), f"Q{qnum}: Expected {name} to be a function, but it is not." 
+        assert hasattr(m,name), f"Q{qnum}: Did you define a function named '{name}'? Did you use the 'def' keyword? Did you use parenthesis?"
+        assert callable(getattr(m,name)), f"Q{qnum}: Expected '{name}' to be a function, but it is not." 
 
         return True
 
@@ -31,61 +31,252 @@ def maketests(m=None):
         global filecontent
         assert re.search(pattern, filecontent), feedback
 
-    @test()
-    def t1a():
+    @test((
+    (3, 1, 2),
+    (0, 0, 0),
+    (4, -1, 5),
+    (300, 100, 200),
+    (-75, -50, -25),
+    ))
+    def t1(*tests):
         n = 1; fun = "add"
         isfunction(n,fun)
         ismatch(
             r"def\s*add\s*\(\s*a\s*,\s*b\s*\):", 
-            f"Q{n}: Is your definition written correcly? Does '{fun}' have parameters 'a' and 'b'?"
+            f"Q{n}: Is your definition written correctly? Does '{fun}' have parameters 'a' and 'b'?"
         )
-        alltests.append(t1b)
-        alltests.append(t2a)
 
-    @test(
-        (3, 1, 2),
-        (0, 0, 0),
-        (4, -1, 5),
-        (300, 100, 200),
-        (-75, -50, -25),
-    )
-    def t1b(*vals):
-        n = 1
-        fun = "add"
-        assert vals[0] == getattr(m,fun)(vals[1],vals[2]), f"Q{n}: Called {fun}({vals[1]},{vals[2]}). Expected {vals[0]}, but got {getattr(m,fun)(vals[1],vals[2])} instead."
+        for t in tests:
+            assert t[0] == getattr(m,fun)(t[1],t[2]), f"Q{n}: Called {fun}({t[1]},{t[2]}). Expected {t[0]}, but got {getattr(m,fun)(t[1],t[2])} instead."
+
         
+    @test()
+    def t2():
+        n = 2; fun = "add"; var = "twosum"; p1 = "a"; p2 ="b"; a1 = 6 ; a2 = 4
+        ismatch(
+            rf"{var}\s*=\s*{fun}\(\s*{a1}\s*,\s*{a2}\s*\)",
+            f"Q{n}: Did you call '{fun}' with the arguments {a1} and {a2}? Are {a1} and {a2} inside the parenthesis and separated by a comma? "
+            f"Did you assign '{var}' to the '{fun}' call?"
+        )
+
+        ismatch(
+            rf"print\(\s*{var}\s*\)",
+            f"Q{n}: Did you print '{var}'?"
+        )
+
+    @test((
+    (1, 3, 2),
+    (0, 5, 5),
+    (-3, 2, 5),
+    (100, 300, 200),
+    (-25, -50, -25),
+    ))
+    def t3(*tests):
+        n = 3; fun = "subtract"; p1 = "minuend"; p2="subtrahend"
+        isfunction(n,fun)
+        ismatch(
+            rf"def\s*{fun}\s*\(\s*{p1}\s*,\s*{p2}\s*\):", 
+            f"Q{n}: Is your definition written correctly? Does '{fun}' have parameters '{p1}' and '{p2}'?"
+        )
+
+        for t in tests:
+            assert t[0] == getattr(m,fun)(t[1],t[2]), f"Q{n}: Called {fun}({t[1]},{t[2]}). Expected {t[0]}, but got {getattr(m,fun)(t[1],t[2])} instead."
+
+    @test()
+    def t4():
+        n = 4; fun = "subtract"; var = "twodiff"; p1 = "minuend"; p2 ="subtrahend"; a1 = 1024 ; a2 = 512
+        ismatch(
+            rf"{var}\s*=\s*{fun}\(\s*{a1}\s*,\s*{a2}\s*\)",
+            f"Q{n}: Did you call '{fun}' with the arguments {a1} and {a2}? Are {a1} and {a2} inside the parenthesis and separated by a comma? "
+            f"Did you assign '{var}' to the '{fun}' call?"
+        )
+
+        ismatch(
+            rf"print\(\s*{var}\s*\)",
+            f"Q{n}: Did you print '{var}'?"
+        )
+
+
+    
+    @test((
+    (6, 3, 2),
+    (25, 5, 5),
+    (10, 2, 5),
+    (60000, 300, 200),
+    (1250, -50, -25),
+    ))
+    def t5(*tests):
+        n = 5; fun = "multiply"; p1 = "x"; p2="y"
+        isfunction(n,fun)
+        ismatch(
+            rf"def\s*{fun}\s*\(\s*{p1}\s*,\s*{p2}\s*\):", 
+            f"Q{n}: Is your definition written correctly? Does '{fun}' have parameters '{p1}' and '{p2}'?"
+        )
+
+        for t in tests:
+            assert t[0] == getattr(m,fun)(t[1],t[2]), f"Q{n}: Called {fun}({t[1]},{t[2]}). Expected {t[0]}, but got {getattr(m,fun)(t[1],t[2])} instead."
+
+    @test()
+    def t6():
+        n = 6; fun = "multiply"; var = "twoproduct"; p1 = "x"; p2 ="y"; a1 = 128 ; a2 = 8
+        ismatch(
+            rf"{var}\s*=\s*{fun}\(\s*{a1}\s*,\s*{a2}\s*\)",
+            f"Q{n}: Did you call '{fun}' with the arguments {a1} and {a2}? Are {a1} and {a2} inside the parenthesis and separated by a comma? "
+            f"Did you assign '{var}' to the '{fun}' call?"
+        )
+
+        ismatch(
+            rf"print\(\s*{var}\s*\)",
+            f"Q{n}: Did you print '{var}'?"
+        )
+
+    @test((
+    (2, 4, 2),
+    (1, 5, 5),
+    (0, 0, 5),
+    (1.5, 300, 200),
+    (2, -50, -25),
+    ))
+    def t7(*tests):
+        n = 7; fun = "divide"; p1 = "dividend"; p2="divisor"
+        isfunction(n,fun)
+        ismatch(
+            rf"def\s*{fun}\s*\(\s*{p1}\s*,\s*{p2}\s*\):", 
+            f"Q{n}: Is your definition written correctly? Does '{fun}' have parameters '{p1}' and '{p2}'?"
+        )
+
+        for t in tests:
+            assert t[0] == getattr(m,fun)(t[1],t[2]), f"Q{n}: Called {fun}({t[1]},{t[2]}). Expected {t[0]}, but got {getattr(m,fun)(t[1],t[2])} instead."
+
+    @test()
+    def t8():
+        n = 8; fun = "divide"; var = "twoquotient"; p1 = "dividend"; p2 ="divisor"; a1 = 65536 ; a2 = 1024
+        ismatch(
+            rf"{var}\s*=\s*{fun}\(\s*{a1}\s*,\s*{a2}\s*\)",
+            f"Q{n}: Did you call '{fun}' with the arguments {a1} and {a2}? Are {a1} and {a2} inside the parenthesis and separated by a comma? "
+            f"Did you assign '{var}' to the '{fun}' call?"
+        )
+        ismatch(
+            rf"print\(\s*{var}\s*\)",
+            f"Q{n}: Did you print '{var}'?"
+        )
+
+    @test((
+    ("hello world", "hello ", "world"),
+    ("123abc", "123", "abc"),
+    ("testcase", "test", "case"),
+    ("foobar", "foo", "bar"),
+    ("2025-04-09", "2025-", "04-09"),
+    ))
+    def t9(*tests):
+        n = 9; fun = "concatenate"; p1 = "front_half"; p2="back_half"
+        isfunction(n,fun)
+        ismatch(
+            rf"def\s*{fun}\s*\(\s*{p1}\s*,\s*{p2}\s*\):", 
+            f"Q{n}: Is your definition written correctly? Does '{fun}' have parameters '{p1}' and '{p2}'?"
+        )
+
+        for t in tests:
+            assert t[0] == getattr(m,fun)(t[1],t[2]), f"Q{n}: Called {fun}({t[1]},{t[2]}). Expected {t[0]}, but got {getattr(m,fun)(t[1],t[2])} instead."
+
+    @test()
+    def t10():
+        n = 10; fun = "concatenate"; var = "joined"; p1 = "front_half"; p2 ="back_half"; a1 = "\"It means \"" ; a2 = "\"to join together.\""
+        ismatch(
+            rf"{var}\s*=\s*{fun}\(\s*{a1}\s*,\s*{a2}\s*\)",
+            f"Q{n}: Did you call '{fun}' with the arguments {a1} and {a2}? Are {a1} and {a2} inside the parenthesis and separated by a comma? "
+            f"Did you assign '{var}' to the '{fun}' call?"
+        )
+
+        ismatch(
+            rf"print\(\s*{var}\s*\)",
+            f"Q{n}: Did you print '{var}'?"
+        )
 
 
     @test()
-    def t2a():
-        n = 2; fun = "add"
+    def t11():
         ismatch(
-            r"twosum\s*=\s*add\(\s*6\s*,\s*4\s*\)",
-            f"Q{n}: Did you call 'add' with the parameters 6 and 4? Are 6 and 4 inside the parenthesis and separated by a comma?"
-            "Did assign 'twosum' to the 'add' call?"
+            r"print\(\s*[\"']Its[\"']\s*,\s*[\"']not[\"']\s*,\s*[\"']a[\"']\s*,\s*[\"']bug[\"']\s*,\s*[\"']its[\"']\s*,\s*[\"']a[\"']\s*,\s*[\"']feature[\"']\s*\)",
+            f"Q11: Did you use only one print call? Did you use commas (,) ? Make sure you are not using 'sep'. Check capitalization and spaces."
+                
         )
-
-        ismatch(
-            r"print\(\s*twosum\s*\)",
-            f"Q{n}: Did you print 'twosum'?"
-        )
-
-
-
-
-    alltests.append(t1a)
     
-    # alltests.append(t2a)
-    # alltests.append(t3a)
-    # alltests.append(t4)
-    # alltests.append(t5)
-    # alltests.append(t6)
-    # alltests.append(t7)
-    # alltests.append(t8)
-    # alltests.append(t9)
-    # alltests.append(t10)
-    # alltests.append(t11)
-    # alltests.append(t12)
+    @test()
+    def t12():
+        words = ("Its ", "a ", "twist ", "ENDing.")
+
+        for word in words:
+            ismatch(
+                r"print\(\s*[\"']"f"{word}"r"[\"']\s*,\s*end\s*=\s*"r"[\"'][\"']\s*\)",
+                f"Q12: Did you use a separate print call? Did you use commas (,) ? Are you setting the 'end' parameter? Check capitalization and spaces." 
+            )
+
+    @test()
+    def t13():
+        ismatch(
+            r"print\(\s*[\"']First[\"']\s*,\s*[\"']Second[\"']\s*,\s*[\"']Third[\"']\s*,\s*[\"']Fourth[\"']\s*,\s*sep\s*=\s*[\"']\|[\"']\s*\)",
+            f"Q13: Did you use only one print call? Did you use commas (,) ? Are you setting the 'sep' parameter? Check capitalization and spaces."
+        )
+
+    @test()
+    def t14():
+        ismatch(
+            r"int\(\s*input\(\s*[\"']Enter a number:\\n[\"']\s*\)\s*\)",
+            f"Q14: Did you use input()? Did you include the correct prompt 'Enter a number:\\n'? Did you convert to an integer with int()? Check capitalization and spaces."
+        )
+
+        assert hasattr(m, "num"), "Q14: Did you define a variable named 'num'?"
+        assert getattr(m, "num") == 2 , "Q14: Did you assign 'num' to your nested int(input()) call?"
+        
+        ismatch(
+            r"print\(\s*num\s*\*\s*5\s*\)",
+            "Q14: Did you print 'num' multiplied by 5? Did you use the multiplication operator? Don't use quotes(\" or ')."
+        )
+
+    @test()
+    def t15():
+        ismatch(
+            r"float\(\s*input\(\s*[\"']Enter a number:\\n[\"']\s*\)\s*\)",
+            f"Q15: Did you use input()? Did you include the correct prompt 'Enter a number:\\n'? Did you convert to a float with float()? Check capitalization and spaces."
+        )
+
+        assert hasattr(m, "num"), "Q15: Did you define a variable named 'flo'?"
+        assert getattr(m, "num") == 2.0 , "Q15: Did you assign 'flo' to your nested float(input()) call?"
+        
+        ismatch(
+            r"print\(\s*flo\s*\*\s*3.14\s*\)",
+            "Q15: Did you print 'flo' multiplied by 3.14? Did you use the multiplication operator? Don't use quotes(\" or ')."
+        )
+    
+
+    @test()
+    def t16():
+        ismatch(
+            r"print\(\s*len\(\s*[\"']abcdefghijklmnopqrstuvwxyz[\"']\s*\)\s*\)",
+            "Q16: Did you print? Did you use the correct length function call? Did you use quotes (\" or ')? Did you use \"abcdefghijklmnopqrstuvwxyz\" as an argument for the length function call?"
+        )
+        ismatch(
+            r"print\(\s*len\(\s*[\"']a b c d e f g h i j k l m n o p q r s t u v w x y z [\"']\s*\)\s*\)",
+            "Q16: Did you print? Did you use the correct length function call? Did you use quotes (\" or ')? Did you use \"a b c d e f g h i j k l m n o p q r s t u v w x y z \" as an argument for the length function call?"
+        )
+
+    alltests.append(t1)
+    alltests.append(t2)
+    alltests.append(t3)
+    alltests.append(t4)
+    alltests.append(t5)
+    alltests.append(t6)
+    alltests.append(t7)
+    alltests.append(t8)
+    alltests.append(t9)
+    alltests.append(t10)
+    alltests.append(t11)
+    alltests.append(t12)
+    alltests.append(t13)
+    alltests.append(t14)
+    alltests.append(t15)
+    alltests.append(t16)
 
 def checkintegrity():
     hasher = hashlib.sha512()
@@ -211,7 +402,7 @@ def assess():
         passpercent = 0
         failpercent = 0
 
-
+    report.insert(0,"\n")
     if numpassed:
         report.insert(0,f"[ Tests Passed \u2705: {numpassed} : {passpercent}% ]\n")
     if numfailed:
@@ -236,8 +427,10 @@ def printreport():
     for r in report:
         if "Test Failed" in r:
             print(r)
+
         else:
             print(r.replace("\n",""))
+
 
 def change_inputfunction(inp=None):
   global filepath,filecontent
